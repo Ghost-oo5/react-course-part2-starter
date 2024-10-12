cc712fa764e83865a5e238a80a69eae2a52e58d6
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2024-10-12 12:19:50 +0500

    practices Load more

--- src/react-query/Hooks/usePost.ts
+++ src/react-query/Hooks/usePost.ts
@@ -2,37 +2,33 @@ import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
-  }
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
 
-  interface PostQuery
-{
-    PageSize:number,
-}  
-const usePost = (query:PostQuery) => useInfiniteQuery<Post[], Error>({
-    queryKey: ['posts', query.PageSize],
-    initialPageParam: 1,
-
-    queryFn: ({pageParam = 1}) =>
+interface PageQuery {
+  pageSize: number;
+}
+const usePosts = (query: PageQuery) => {
+  return useInfiniteQuery<Post[]>({
+    queryKey: ["posts", query.pageSize],
+    queryFn: ({ pageParam = 1 }) =>
       axios
-        .get<Post[]>("https://jsonplaceholder.typicode.com/posts",
-            {
-                params:{
-                   _start: (pageParam -1 )*query.PageSize,
-                   _limit: query.PageSize 
-                }
-            }
-        )
+        .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: (pageParam - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
+        })
         .then((res) => res.data),
-        getNextPageParam:(lastPage, allPages)=>
-        {
-            return lastPage.length > 0? allPages.length+1:undefined;
-        }
-    
+    staleTime: 3 * 1000,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
+};
 
-
-export default usePost;
+export default usePosts;
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,34 +1,26 @@
-import { useState } from "react";
-import usePost from "./Hooks/usePost";
+import usePosts from "./Hooks/usePost";
 import React from "react";
 
 const PostList = () => {
-  const PageSize= 10;
-  // const [page, setPage] =useState(1);
-  const { data, error, isLoading, fetchNextPage } = usePost({PageSize});
+  const pageSize=10;
+  const { data, error, isLoading, fetchNextPage } = usePosts({ pageSize });
 
-  if (error) return <p>{error.message}</p>;
-  if (isLoading) return <div className="spinner-border text-warning"></div>;
-
+  if (error) return <p className="text-danger fw-bold">{error.message}</p>;
+  if (isLoading) return <div className="spinner-border text-success"></div>;
   return (
     <>
-      
       <ul className="list-group">
-        {data?.pages.map((page, index)=>
+        {data?.pages.map((page, index) => (
           <React.Fragment key={index}>
-            {page.map((post) => (
-          <li key={post.id} className="list-group-item text-success">
-            {post.title}
-          </li>
-        ))}
+            {page?.map((post) => (
+              <li key={post.id} className="list-group-item text-danger">
+                {post.id} {post.title}
+              </li>
+            ))}
           </React.Fragment>
-        )}
-        
+        ))}
       </ul>
-      {/* <button
-      disabled={page==1} type="button" className="btn btn-primary me-2 my-3" onClick={()=>setPage(page-1)}>Prev</button>
-      <button type="button" className="btn btn-primary my-3" onClick={()=>setPage(page+1)}>Next</button> */}
-      <button type="button" className="btn btn-primary my-3" onClick={()=>fetchNextPage()}>Load more</button>
+      <button className="btn btn-primary my-3" onClick={()=>fetchNextPage()}>Load more</button>
     </>
   );
 };
