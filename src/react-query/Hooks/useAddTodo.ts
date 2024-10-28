8bc1705d4a3079d6e25c94cee83e0daa96605d7f
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2024-10-28 21:01:44 +0500

    Created a Reusable API Client

--- src/react-query/Hooks/useAddTodo.ts
+++ src/react-query/Hooks/useAddTodo.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 import React from 'react'
 import { Todo } from './useTodos';
 import { CACHE_KEY_TODOS } from '../Constants';
+import APIClient from '../../Services/api-client';
+
+const apiClient = new APIClient<Todo>('/todos')
 
 interface addTodoContext {
     previousTodos: Todo[];
@@ -12,10 +15,7 @@ function useAddTodo(onAdd:()=>void) {
     const query = useQueryClient();
 
     return useMutation<Todo, Error, Todo, addTodoContext>({
-        mutationFn: (todo: Todo) =>
-          axios
-            .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
-            .then((res) => res.data),
+        mutationFn: apiClient.postAll,
         onMutate: (newTodo: Todo) => {
           const previousTodos = query.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
           query.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos=[]) => [
--- src/react-query/Hooks/useTodos.ts
+++ src/react-query/Hooks/useTodos.ts
@@ -2,6 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
 import { CACHE_KEY_TODOS } from "../Constants";
+import APIClient from "../../Services/api-client";
+
+const apiClient = new APIClient('/todos')
 
 export interface Todo {
   id: number;
@@ -10,11 +13,8 @@ export interface Todo {
 
 function useTodos() {
   return useQuery({
-    queryKey: CACHE_KEY_TODOS,
-    queryFn: () =>
-      axios
-        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-        .then((res) => res.data),
+    queryKey: CACHE_KEY_TODOS, 
+    queryFn: apiClient.getAll,
   });
 }
 export default useTodos;
